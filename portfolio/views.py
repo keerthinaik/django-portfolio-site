6e2f986e10d7cf274b77b9982f721531bc88da0e
KEERTHINAIK R <keerthinaik.harika@gmail.com>
2020-05-20 23:39:32 +0530

    bootstrapped all html pages and fixed blog views

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,4 +3,5 @@ from portfolio.models import Project
 
 def home(request):
     projects = Project.objects.all()
-    return render(request,'portfolio/home.html',{'projects':projects})
+    context = {'projects':projects}
+    return render(request,'portfolio/home.html',context)
